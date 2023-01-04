759c722dcce304497462520c76887aacad620ed8
borossandor27 <boros.sandor@dszcberegszaszi.hu>
2023-01-04 13:30:33 +0100

    Async

--- PeopleRestClient/app/src/main/java/com/example/peoplerestclient/MainActivity.java
+++ PeopleRestClient/app/src/main/java/com/example/peoplerestclient/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.peoplerestclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,21 +15,43 @@ public class MainActivity extends AppCompatActivity {
     private EditText nameInput, emailInput, ageInput;
     private Button submitButton;
     private TextView peopleTextview;
+    //-- retool.com/api-generator ---------
     private final String base_url = "https://retoolapi.dev/cRJhEP/people";
+
+    /*
+    This class was deprecated in API level 30.
+    Use the standard java.util.concurrent
+        or
+        Kotlin concurrency utilities (https://developer.android.com/topic/libraries/architecture/coroutines) instead.
+     */
+    private class RequestTask extends AsyncTask<Void, Void, Response> {
+
+
+        @Override
+        protected Response doInBackground(Void... voids) {
+            try {
+                Response response = RequestHandler.get(base_url);
+                String people = response.getContent();
+                //-- other thread ---
+                runOnUiThread(() -> {
+                    peopleTextview.setText(people);
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        RequestTask task = new RequestTask();
+        task.execute();
 
-        try {
-            Response response = RequestHandler.get(base_url);
-            String people = response.getContent();
-            peopleTextview.setText(people);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     private void init() {
